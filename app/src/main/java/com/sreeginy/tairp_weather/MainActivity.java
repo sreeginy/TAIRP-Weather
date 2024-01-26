19396742bfc03d6f2c332993df1efe355682725f
Sreeginy <sreeginy98@gmail.com>
2024-01-26 21:38:48 +0530

    update API

--- app/src/main/java/com/sreeginy/tairp_weather/MainActivity.java
+++ app/src/main/java/com/sreeginy/tairp_weather/MainActivity.java
@@ -2,9 +2,33 @@ package com.sreeginy.tairp_weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView date;
+    String apiKey = "0834e2dbfe812be60ce5bb46a74aa17c";
+    String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=Colombo&appid=" + apiKey;
+
+    final long MIN_TIME = 5000;
+    final float MIN_DISTANCE = 1000;
+    final int REQUEST_CODE = 101;
+
+    String locationProvider = LocationManager.GPS_PROVIDER;
+
+    TextView mNameOfCity, mWeatherType, mTemperature, rain, windSpeed, humidity;
+    ImageView mWeatherIcon;
+    Button searchBtn;
+    LocationManager locationManager;
+    LocationListener locationListener;
+    ImageButton viewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
